1ebda3a652667750793ed018c1c5f55585ded603
trainocate-repo <elproj@trainocate.co.jp>
2019-11-07 18:59:25 +0900

    add auth

--- resetdb.php
+++ resetdb.php
@@ -1,5 +1,5 @@
 <?php
-require_once('../config/db_config.php');
+require_once('./config/db_config.php');
 header('Content-Type: text/html; charset=UTF-8');
 
 $db = new PDO(DSN, DB_USER, DB_PASSWORD, $opt);
@@ -9,6 +9,7 @@ $sql = <<<EOS
 
 DROP TABLE IF EXISTS books;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS members;
 
 CREATE TABLE books (
     id INT(10),
@@ -31,6 +32,15 @@ INSERT INTO books VALUES (4, 'AI概論','田中涼子',3100);
 INSERT INTO users VALUES (1, 'suzuki@example.jp', 'pass');
 INSERT INTO users VALUES (2, 'takana@example.com','pass123');
 INSERT INTO users VALUES (3, 'sato@example.net', 'password');
+
+CREATE TABLE members (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255) UNIQUE,
+    password VARCHAR(255),
+    created DATETIME,
+    modified DATETIME
+);
 
 EOS;
 $db->query($sql);
